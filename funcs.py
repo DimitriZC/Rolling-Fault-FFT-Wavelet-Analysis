a88e8595a2744bf526519d9535b0fb9a92eaf16e
Dimitri Zuave <dimitrizuave14@gmail.com>
2024-09-22 15:23:27 -0300

    strucured

--- funcs.py
+++ funcs.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import scipy.io
 import pywt
+
+from scipy.signal import hilbert, freqz
 from matplotlib import pyplot as plt
 
 from typing import List, Tuple
@@ -9,12 +11,12 @@ from typing import List, Tuple
 
 def Load(file: str) -> Tuple[List]:
 
-    Rolling_bsln_1 = scipy.io.loadmat(os.path.join('Data', file))
-    x = Rolling_bsln_1['bearing']['gs'][0][0]
-    fs = Rolling_bsln_1['bearing']['sr'][0][0][0]
+    DATA = scipy.io.loadmat(os.path.join('Data', file))
+    x = DATA['bearing']['gs'][0][0]
+    fs = DATA['bearing']['sr'][0][0][0]
     time = np.transpose(np.arange(0, len(x)) / fs)
 
-    return x, fs, time
+    return x, fs, time, DATA
 
 
 def PSD(f: List, time: List, num: int) -> Tuple[List]:
@@ -27,6 +29,17 @@ def PSD(f: List, time: List, num: int) -> Tuple[List]:
     L = np.arange(1, np.floor(n/2), dtype='int')
 
     return freq, psd, L
+
+def plot_psd(freq, psd, L, title=''):
+
+    plt.figure()
+    plt.plot(freq[L], psd[L])
+    plt.xlim(freq[L[0]], freq[L[-1]])
+    plt.xlabel('Freq [Hz]')
+    plt.ylabel('Power Spectral Density')
+    plt.title(f'PSD: {title}')
+
+
 
 
 def Spec(f: List, fs: int, title: str=""):
@@ -55,3 +68,20 @@ def wavelet_filter(coefficients, wavelet) -> List:
     Arecon = pywt.waverec(coeffs_filt,wavelet=wavelet)
 
     return Arecon
+
+
+def env_spectrum(x, fs):
+
+    analytic_signal = hilbert(x)
+    
+    envelope = np.abs(analytic_signal)
+    
+    f_env = np.fft.fftfreq(len(envelope), d=1/fs)
+    p_env = np.abs(np.fft.rfft(envelope) / len(x))
+    
+    
+    x_env = envelope
+    
+    return p_env, f_env, x_env, 
+
+
--- main.py
+++ main.py
@@ -1,34 +1,83 @@
 import numpy as np
+import pywt
 import matplotlib.pyplot as plt
 
 
-from funcs import PSD, Spec, Load
+from funcs import PSD, Spec, Load, wavelet_filter, env_spectrum, plot_psd
 
 file_to_load = 'InnerRaceFault_vload_1.mat'
-
-x_IRF_1_1, f_IRF_1_1, t_IRF_1_1 = Load(file_to_load)
-
+title = file_to_load.split('.')[0]
 
 
-num = int(len(t_IRF_1_1)*0.0005)
+x_IRF_1_1, f_IRF_1_1, t_IRF_1_1, IRFDATA = Load(file_to_load)
+
+
+
+num = int(len(t_IRF_1_1)*0.001)
 
 freq, psd, L = PSD(x_IRF_1_1, t_IRF_1_1, num)
 
-plt.plot(freq[L], psd[L])
-plt.xlim(freq[L[0]], freq[L[-1]])
-plt.xlabel('Freq [Hz]')
-plt.ylabel('Power Spectral Density')
-plt.title('Power Spectral Density (PSD)')
+plot_psd(freq, psd, L, title)
 
-Spec(np.transpose(x_IRF_1_1[:num])[0], f_IRF_1_1[0], title=file_to_load.split('.')[0])
+Spec(np.transpose(x_IRF_1_1[:num])[0], f_IRF_1_1[0], title)
 
 file_to_load = 'baseline_1.mat'
+title = file_to_load.split('.')[0]
 
-x_bsln_1, f_bsln_1, t_bsln_1 = Load(file_to_load)
+x_bsln_1, f_bsln_1, t_bsln_1, BSLN_DATA = Load(file_to_load)
 num = int(len(t_bsln_1)*0.0005)
 
-Spec(np.transpose(x_bsln_1[:num])[0], f_bsln_1[0], title=file_to_load.split('.')[0])
+freq, psd, L = PSD(x_bsln_1, t_bsln_1, num)
 
+plot_psd(freq, psd, L, title)
+
+
+Spec(np.transpose(x_bsln_1[:num])[0], f_bsln_1[0], title)
+
+
+
+#Wavelet test
+
+wavelet='db1'
+scales = (1, len(x_bsln_1))
+coeffs = pywt.wavedec(x_bsln_1, wavelet)
+
+recon = wavelet_filter(coeffs, wavelet)
+title = "Recon baseline"
+
+freq, psd, L = PSD(recon, t_bsln_1, num)
+
+plot_psd(freq, psd, L, title)
+
+Spec(np.transpose(recon[:num])[0], f_bsln_1[0], title='Recon')
+
+p_env_inner, f_env_inner, x_env_inner = env_spectrum(np.transpose(x_IRF_1_1[:])[0],  f_IRF_1_1[0])
+
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.xlim(0.04, 0.06)
+plt.plot(t_IRF_1_1, x_IRF_1_1)
+plt.xlabel('Time [s]')
+plt.ylabel('Acceletarion [g]')
+num = int(len(t_IRF_1_1)*0.001)
+plt.subplot(2, 1, 2)
+plt.plot(t_IRF_1_1[:len(x_env_inner)], x_env_inner)
+plt.xlim(0.04, 0.06)
+# plt.ylim(0, 1)
+plt.xlabel('Time [s]')
+plt.ylabel('Acceletarion [g]')
+
+
+plt.figure()
+a = 3500
+plt.plot(f_env_inner[:a], p_env_inner[:a])
+plt.xlim(0, 1000)
+plt.ylim( 0, 0.5)
+
+ncomb = 10
+harmonics = [k * IRFDATA["BPFI"] for k in range(1, ncomb + 1)]
+
+plt.vlines(harmonics, 0, max(p_env_inner[:a]), colors='r', linestyles='--')
 
 
 plt.show()
