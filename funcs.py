95fe584391171c5392b8097ace61a38341dd70fa
DimitriZc <dimitrizuave14@gmail.com>
2024-09-25 17:17:37 -0300

    WORKIIIIING: missing docs

--- funcs.py
+++ funcs.py
@@ -76,31 +76,30 @@ def band_pass(signal, low, high, fs, order=5):
     low = low / nyquist
     high = high / nyquist
 
-    bpf_coefficients = firwin(5 + 1, [low[0], high[0]], pass_zero=False)
+    bpf_coefficients = firwin(50 + 1, [low, high], pass_zero=False)
     b, a = butter(order, [low, high], fs=fs, btype='band')
-    return lfilter(bpf_coefficients, 1.0,  signal)
+    return lfilter(bpf_coefficients, 1.0, signal)
 
 
-def env_spectrum(x, fs, t, BAND=False, LOW=0, HIGH=0):
-    '''
-    with open("../supsub/x.csv", 'w') as f:
-        f.write('time,x\n')
-        for i, a in enumerate(x):
-            f.write(f"{t[i]},{a}\n")
-    '''
-    if BAND:
-        if not LOW or not HIGH:
-            raise AttributeError("Frequency Band not determined. Can't be zero!")
-        x = band_pass(x, LOW, HIGH, fs[0])
+def env_spectrum(x, fs, time, ba=0):
 
-    analytic_signal = hilbert(np.abs(x))
-    envelope = np.abs(analytic_signal)
+
+    # analytic_signal = hilbert(x)
+    # envelope = np.abs(analytic_signal)
+
+    envelope = complex_demod(x, fs, time, ba)
+
+    mean_envelope = np.mean(envelope)
+
+
+
+    # Remove DC bias by subtracting the mean from the envelope
+    envelope = envelope - mean_envelope
 
     # high, _ = hl_envelopes_idx(np.abs(x), dmin=2)
 
     # envelope = np.abs(x[high])
     # t_env = t[high]
-    t_env=t
 
 
     # y_d = np.gradient(np.abs(x))
@@ -109,15 +108,42 @@ def env_spectrum(x, fs, t, BAND=False, LOW=0, HIGH=0):
     
     
     f_env = np.fft.fftfreq(len(envelope), d=1/fs)
-    p_env = np.abs(np.fft.rfft(envelope) / len(x))
+    p_env = np.abs(np.fft.fft(envelope))/len(envelope)
     
     
     x_env = envelope
+
+        # Compute one-sided spectrum. Compensate the amplitude for a two-sided
+    # spectrum. Double all points except DC and nyquist.
+    if len(p_env) % 2 != 0:
+        # Odd length two-sided spectrum
+        f_env = f_env[1:int((len(f_env) + 1)/2)]
+        p_env = p_env[1:int((len(p_env)+1)/2)]
+        p_env[2:int(len(p_env))] = 2*p_env[2:int(len(p_env))]
+    else:
+        # Even length two-sided spectrum
+        f_env = f_env[1:int(len(f_env)/2+1)]
+        p_env = p_env[1:int(len(f_env)/2+1)]
+        p_env[2:int(len(p_env)-1)] = 2*p_env[2:int(len(p_env)-1)]
     
-    return p_env, f_env, x_env, t_env
+    
+    return p_env, f_env, x_env
 
 
-def plot_env_spectrum(x_env, time_env, x_raw, time, HIGH=[0], xlim=[0.04, 0.06]):
+def complex_demod(signal, fs, time, ba=0, order=50):
+
+    if ba == 0:
+        ba = [fs/4, 3/8*fs]
+    f0 = (ba[1] + ba[0])/2
+    x0 = signal*np.exp(-1j*2*np.pi*f0*time)
+    b = firwin(order, (ba[1] - ba[0])/2/(fs/2))
+    xAn = scipy.signal.convolve(x0, b, mode='same')
+    xAn = 2*xAn
+    return np.abs(xAn)
+
+
+
+def plot_env_spectrum(x_env, x_raw, time, xlim=[0.04, 0.06]):
 
 
     plt.figure()
@@ -127,11 +153,8 @@ def plot_env_spectrum(x_env, time_env, x_raw, time, HIGH=[0], xlim=[0.04, 0.06])
     plt.xlabel('Time [s]')
     plt.ylabel('Acceletarion [g]')
     plt.subplot(2, 1, 2)
-    if len(HIGH) > 1:
-        time = time_env
     plt.plot(time, x_env)
     plt.xlim(xlim)
-    # plt.ylim(0, 1)
     plt.xlabel('Time [s]')
     plt.ylabel('Acceletarion [g]')
 
@@ -147,7 +170,8 @@ def plot_env_spectrum_analysis(f_env, p_env, BP, BPFI=False, BPFO=False, ALL=Fal
     plt.xlabel("Frequency [Hz]")
     plt.ylabel('Peak Amplitude')
     plt.xlim(0, 1000)
-    plt.ylim( 0, 0.5)
+    plt.ylim(0, max(p_env[3:])*1.1)
+
 
     if BPFI:
         values = []
@@ -155,14 +179,14 @@ def plot_env_spectrum_analysis(f_env, p_env, BP, BPFI=False, BPFO=False, ALL=Fal
             values.append(k * BP["BPFI"])
             k +=1
         harmonics = dict(values=values, name="BPFI")
-        plt.vlines(harmonics['values'], 0, 0.5, colors='r', linestyles='--', label=harmonics['name'])
+        plt.vlines(harmonics['values'], 0, max(p_env[3:])*1.1, colors='r', linestyles='--', label=harmonics['name'])
     if BPFO:
         values = []
         while ncomb * BP["BPFO"] < 1000: 
             values.append(ncomb * BP["BPFO"])
             ncomb +=1
         harmonics = dict(values=values, name="BPFO")
-        plt.vlines(harmonics['values'], 0, 0.5, colors='#f87915', linestyles='--', label=harmonics['name'])
+        plt.vlines(harmonics['values'], 0, max(p_env[3:])*1.1, colors='#f87915', linestyles='--', label=harmonics['name'])
 
     plt.legend(framealpha=1)
 
@@ -198,4 +222,72 @@ def hl_envelopes_idx(s, dmin=1, dmax=1, split=False):
     lmax = lmax[[i+np.argmax(s[lmax[i:i+dmax]]) for i in range(0,len(lmax),dmax)]]
     
     return lmin,lmax
+
+
+def plot_kurtogram(x, fs) -> Tuple[float]:
+
+    Kwav, Level_w, freq_w, fc, max_Kurt, bandwidth, level_max = fast_kurtogram(x, fs, 9)   # Center frequency & bandwidth obtained from kurtogram
+    minw = np.where(Level_w == level_max)[0][0]
+    kurtw = np.where(Kwav[minw, :] == max_Kurt)[0][0]
+    bandw = freq_w[kurtw]
+    plt.figure()
+    ax = plt.gca()
+    im = ax.imshow(Kwav, interpolation='none', aspect='auto')
+    xlavel = np.array(np.arange(0, fs/2+fs/2/7, fs/2/7), dtype=int)
+    plt.xticks(np.arange(0, Kwav.shape[1], Kwav.shape[1] // 7), labels=xlavel)
+    plt.title(f'K max ={max_Kurt:.4f} at level {level_max:.1f}\nCenter frequency : {bandw + bandwidth/2:.1f}Hz Bandwidth ={bandwidth:.1f}Hz')
+    plt.xlabel('frequency (Hz)')
+    plt.yticks(np.arange(0, Kwav.shape[0], 1), labels=np.round(Level_w, 1))
+    plt.ylabel('level (window lenght)')
+
+    plt.colorbar(im)
+
+    plt.ylabel('Spectral Kurtosis')
+    plt.tight_layout()
+
+    return bandw, bandwidth
+
+
+def complete_analysis(file: str, BPFO: bool = False, BPFI: bool = False, ALL: bool = False):
+    title = file.split('.')[0]
+    std_xlim = [0, 0.1]
+    x_raw, f_raw, t_raw, DATA_raw = Load(file)
+
+    p_env_raw, f_env_raw, x_env_raw =  env_spectrum(np.transpose(x_raw)[0], f_raw[0], t_raw)
+
+    plot_env_spectrum((x_env_raw), x_raw, t_raw, xlim=std_xlim)
+    plt.savefig(f"imgs/{title}_raw_env.png")
+
+    plot_env_spectrum_analysis(f_env_raw, p_env_raw, DATA_raw, BPFO=BPFO, BPFI=BPFI, ALL=ALL)
+    plt.title(f'Raw {title} Signal')
+    plt.savefig(f"imgs/{title}_raw_envSpec.png")
+
+
+    # Kurtogram analysis
+
+    bandw, BW = plot_kurtogram(np.transpose(x_raw)[0], f_raw[0])
+    plt.savefig(f"imgs/{title}_kurtogram.png")
+
+    fc = bandw + BW/2
+
+    low = fc - BW/2
+    high = fc + BW/2
+
+    x_filtered = band_pass(np.transpose(x_raw)[0], low, high, f_raw[0])
+
+    p_env_filtered, f_env_filtered, x_env_filtered =  env_spectrum(np.transpose(x_raw)[0], f_raw[0], t_raw, ba=[low, high])
+
+    plot_env_spectrum(x_env_filtered, x_raw, t_raw, xlim=std_xlim)
+    plt.savefig(f"imgs/{title}_filt_env.png")
+
+
+
+    plot_env_spectrum_analysis(f_env_filtered, p_env_filtered, DATA_raw, BPFO=BPFO, BPFI=BPFI, ALL=ALL)
+    plt.title(f'Filtered {title} Signal')
+    plt.savefig(f"imgs/{title}_filt_envSpec.png")
+
+    plt.close()
+
+
+
 
--- main.py
+++ main.py
@@ -1,71 +1,31 @@
-import os
-import numpy as np
-import scipy.io
-import pywt
-
-from scipy.signal import hilbert, butter, lfilter, firwin
-from matplotlib import pyplot as plt
-
-from typing import List, Tuple
-from kurtogram import fast_kurtogram
+from pathlib import Path
 from funcs import *
 
-# from scipy.signal import butter, lfilter
+directory = Path('DATA/')
 
 
-# def band_pass(signal, low, high, fs, order=5):
-#     b, a = butter(N=order, Wn=[low, high], fs=fs, btype='band')
-#     return lfilter(b, a, signal)
-file_to_load = 'OuterRaceFault_1.mat'
-title = file_to_load.split('.')[0]
+for file_path in directory.glob('*.mat'):
+    file = str(file_path).split('\\')[1]
+    if 'baseline' in file:
+        ALL = True
+        BPFI = False
+        BPFO = False
+    elif 'Inner' in file:
+        ALL = False
+        BPFI = True
+        BPFO = False
+    elif 'Outer' in file:
+        ALL = False
+        BPFI = False
+        BPFO = True
+    else:
+        raise AttributeError('Invalid File Name')
 
-x_ORF_2, f_ORF_2, t_ORF_2, ORF_DATA = Load(file_to_load)
-num = int(len(t_ORF_2)*0.0005)
+    complete_analysis(file, BPFI=BPFI, BPFO=BPFO, ALL=ALL)
 
-freq, psd, L = PSD(x_ORF_2, t_ORF_2, num)
+# complete_analysis('OuterRaceFault_2.mat', BPFO=True)
 
-p_env, f_env, x_env, t_env = env_spectrum(np.transpose(x_ORF_2)[0],  f_ORF_2[0], t_ORF_2)
-
-plot_env_spectrum(x_env, t_env, x_ORF_2, t_ORF_2, xlim=[0, 0.1])
-
-plot_env_spectrum_analysis(f_env, p_env, ORF_DATA, BPFO=True)
-plt.ylim(0, 0.2)
-
-Kwav, Level_w, freq_w, c, max_Kurt, BW, level_max = fast_kurtogram(x_ORF_2, f_ORF_2, 9)   # Center frequency & bandwidth obtained from kurtogram
-minw = np.where(Level_w == level_max)[0][0]
-kurtw = np.where(Kwav[minw, :] == max_Kurt)[0][0]
-bandw = freq_w[kurtw]
-
-fc = bandw + BW[0]/2
-
-low = fc - BW/2
-high = fc + BW/2
-
-## ERRADO: VER COMO APLICAR O FILTRO DE FREQ NESSE SINAL Q TA NO TEMPO (NO MATLAB TA ASSIM)
-x_ORF_2_Bpf = band_pass(np.transpose(x_ORF_2)[0], low, high, f_ORF_2[0], order=2)
-
-p_env_ORF_2_Bpf, f_env_ORF_2_Bpf, x_env_ORF_2_Bpf, t_env_ORF_Bpf = env_spectrum(np.transpose(x_ORF_2)[0], f_ORF_2, t_ORF_2, BAND=True, LOW=low, HIGH=high)
+print('\n>> DONE!')
 
 
-
-# plot_env_spectrum(x_env, x_ORF_2, t_ORF_2)
-# plot_env_spectrum(x_env_ORF_2_Bpf, x_ORF_2_Bpf, t_ORF_2)
-
-
-# plot_env_spectrum_analysis(f_env_ORF_2_Bpf, p_env_ORF_2_Bpf, ORF_DATA, BPFO=True)
-
-high, _= hl_envelopes_idx(np.transpose(x_ORF_2)[0])
-# plot_env_spectrum(x_env, t_env, x_ORF_2, t_ORF_2, HIGH=high, xlim=[0, 0.1])
-# plt.ylim(-1, 1)
-
-
-# high, _= hl_envelopes_idx(x_ORF_2_Bpf)
-plot_env_spectrum(x_ORF_2_Bpf, t_env_ORF_Bpf, x_ORF_2, t_ORF_2, HIGH=high, xlim=[0, 0.1])
-# plt.ylim(-1, 1)
-
-plot_env_spectrum_analysis(f_env_ORF_2_Bpf, p_env_ORF_2_Bpf, ORF_DATA,BPFO=True)
-plt.ylim(0, 0.2)
-
-
-plt.show()
-
+# plt.show()
