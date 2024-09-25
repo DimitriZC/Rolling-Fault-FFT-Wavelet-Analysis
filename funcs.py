5bce82fa1177aaa5dd4dea0f4e52db939fe2a94f
DimitriZc <dimitrizuave14@gmail.com>
2024-09-25 14:10:58 -0300

    BPFO ex WORKED!

--- funcs.py
+++ funcs.py
@@ -3,11 +3,13 @@ import numpy as np
 import scipy.io
 import pywt
 
-from scipy.signal import hilbert, freqz
+from scipy.signal import hilbert, butter, lfilter, firwin
 from matplotlib import pyplot as plt
 
 from typing import List, Tuple
+from kurtogram import fast_kurtogram
 
+from typing import List, Tuple
 
 def Load(file: str) -> Tuple[List]:
 
@@ -56,7 +58,7 @@ def wavelet_filter(coefficients, wavelet) -> List:
 
     Csort = np.sort(np.abs(coeff_arr.reshape(-1)))
 
-    keep = 0.1
+    keep = 0.05
 
     thresh = Csort[int(np.floor((1-keep)*len(Csort)))]
     ind = np.abs(coeff_arr) > thresh
@@ -69,12 +71,42 @@ def wavelet_filter(coefficients, wavelet) -> List:
 
     return Arecon
 
+def band_pass(signal, low, high, fs, order=5):
+    nyquist = 0.5*fs
+    low = low / nyquist
+    high = high / nyquist
 
-def env_spectrum(x, fs):
+    bpf_coefficients = firwin(5 + 1, [low[0], high[0]], pass_zero=False)
+    b, a = butter(order, [low, high], fs=fs, btype='band')
+    return lfilter(bpf_coefficients, 1.0,  signal)
 
-    analytic_signal = hilbert(x)
-    
+
+def env_spectrum(x, fs, t, BAND=False, LOW=0, HIGH=0):
+    '''
+    with open("../supsub/x.csv", 'w') as f:
+        f.write('time,x\n')
+        for i, a in enumerate(x):
+            f.write(f"{t[i]},{a}\n")
+    '''
+    if BAND:
+        if not LOW or not HIGH:
+            raise AttributeError("Frequency Band not determined. Can't be zero!")
+        x = band_pass(x, LOW, HIGH, fs[0])
+
+    analytic_signal = hilbert(np.abs(x))
     envelope = np.abs(analytic_signal)
+
+    # high, _ = hl_envelopes_idx(np.abs(x), dmin=2)
+
+    # envelope = np.abs(x[high])
+    # t_env = t[high]
+    t_env=t
+
+
+    # y_d = np.gradient(np.abs(x))
+    # analytic_signal = hilbert(y_d)
+    # envelope = np.abs(analytic_signal)
+    
     
     f_env = np.fft.fftfreq(len(envelope), d=1/fs)
     p_env = np.abs(np.fft.rfft(envelope) / len(x))
@@ -82,6 +114,88 @@ def env_spectrum(x, fs):
     
     x_env = envelope
     
-    return p_env, f_env, x_env, 
+    return p_env, f_env, x_env, t_env
 
+
+def plot_env_spectrum(x_env, time_env, x_raw, time, HIGH=[0], xlim=[0.04, 0.06]):
+
+
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    plt.xlim(xlim)
+    plt.plot(time, x_raw)
+    plt.xlabel('Time [s]')
+    plt.ylabel('Acceletarion [g]')
+    plt.subplot(2, 1, 2)
+    if len(HIGH) > 1:
+        time = time_env
+    plt.plot(time, x_env)
+    plt.xlim(xlim)
+    # plt.ylim(0, 1)
+    plt.xlabel('Time [s]')
+    plt.ylabel('Acceletarion [g]')
+
+def plot_env_spectrum_analysis(f_env, p_env, BP, BPFI=False, BPFO=False, ALL=False):
+    
+    k = 0
+    ncomb = 0
+    if ALL: BPFI = BPFO = True
+
+    plt.figure()
+    a = next((index for index, value in enumerate(f_env) if value > 1000), -1)
+    plt.plot(f_env[3:a], p_env[3:a], label='Envelope Spectrum')
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel('Peak Amplitude')
+    plt.xlim(0, 1000)
+    plt.ylim( 0, 0.5)
+
+    if BPFI:
+        values = []
+        while k * BP["BPFI"] < 1000: 
+            values.append(k * BP["BPFI"])
+            k +=1
+        harmonics = dict(values=values, name="BPFI")
+        plt.vlines(harmonics['values'], 0, 0.5, colors='r', linestyles='--', label=harmonics['name'])
+    if BPFO:
+        values = []
+        while ncomb * BP["BPFO"] < 1000: 
+            values.append(ncomb * BP["BPFO"])
+            ncomb +=1
+        harmonics = dict(values=values, name="BPFO")
+        plt.vlines(harmonics['values'], 0, 0.5, colors='#f87915', linestyles='--', label=harmonics['name'])
+
+    plt.legend(framealpha=1)
+
+
+def hl_envelopes_idx(s, dmin=1, dmax=1, split=False):
+    """
+    Function from: https://stackoverflow.com/questions/34235530/how-to-get-high-and-low-envelope-of-a-signal
+    
+    Input :
+    s: 1d-array, data signal from which to extract high and low envelopes
+    dmin, dmax: int, optional, size of chunks, use this if the size of the input signal is too big
+    split: bool, optional, if True, split the signal in half along its mean, might help to generate the envelope in some cases
+    Output :
+    lmin,lmax : high/low envelope idx of input signal s
+    """
+
+    # locals min      
+    lmin = (np.diff(np.sign(np.diff(s))) > 0).nonzero()[0] + 1 
+    # locals max
+    lmax = (np.diff(np.sign(np.diff(s))) < 0).nonzero()[0] + 1 
+    
+    if split:
+        # s_mid is zero if s centered around x-axis or more generally mean of signal
+        s_mid = np.mean(s) 
+        # pre-sorting of locals min based on relative position with respect to s_mid 
+        lmin = lmin[s[lmin]<s_mid]
+        # pre-sorting of local max based on relative position with respect to s_mid 
+        lmax = lmax[s[lmax]>s_mid]
+
+    # global min of dmin-chunks of locals min 
+    lmin = lmin[[i+np.argmin(s[lmin[i:i+dmin]]) for i in range(0,len(lmin),dmin)]]
+    # global max of dmax-chunks of locals max 
+    lmax = lmax[[i+np.argmax(s[lmax[i:i+dmax]]) for i in range(0,len(lmax),dmax)]]
+    
+    return lmin,lmax
 
--- main.py
+++ main.py
@@ -1,83 +1,71 @@
+import os
 import numpy as np
+import scipy.io
 import pywt
-import matplotlib.pyplot as plt
+
+from scipy.signal import hilbert, butter, lfilter, firwin
+from matplotlib import pyplot as plt
+
+from typing import List, Tuple
+from kurtogram import fast_kurtogram
+from funcs import *
+
+# from scipy.signal import butter, lfilter
 
 
-from funcs import PSD, Spec, Load, wavelet_filter, env_spectrum, plot_psd
-
-file_to_load = 'InnerRaceFault_vload_1.mat'
+# def band_pass(signal, low, high, fs, order=5):
+#     b, a = butter(N=order, Wn=[low, high], fs=fs, btype='band')
+#     return lfilter(b, a, signal)
+file_to_load = 'OuterRaceFault_1.mat'
 title = file_to_load.split('.')[0]
 
+x_ORF_2, f_ORF_2, t_ORF_2, ORF_DATA = Load(file_to_load)
+num = int(len(t_ORF_2)*0.0005)
 
-x_IRF_1_1, f_IRF_1_1, t_IRF_1_1, IRFDATA = Load(file_to_load)
+freq, psd, L = PSD(x_ORF_2, t_ORF_2, num)
 
+p_env, f_env, x_env, t_env = env_spectrum(np.transpose(x_ORF_2)[0],  f_ORF_2[0], t_ORF_2)
 
+plot_env_spectrum(x_env, t_env, x_ORF_2, t_ORF_2, xlim=[0, 0.1])
 
-num = int(len(t_IRF_1_1)*0.001)
+plot_env_spectrum_analysis(f_env, p_env, ORF_DATA, BPFO=True)
+plt.ylim(0, 0.2)
 
-freq, psd, L = PSD(x_IRF_1_1, t_IRF_1_1, num)
+Kwav, Level_w, freq_w, c, max_Kurt, BW, level_max = fast_kurtogram(x_ORF_2, f_ORF_2, 9)   # Center frequency & bandwidth obtained from kurtogram
+minw = np.where(Level_w == level_max)[0][0]
+kurtw = np.where(Kwav[minw, :] == max_Kurt)[0][0]
+bandw = freq_w[kurtw]
 
-plot_psd(freq, psd, L, title)
+fc = bandw + BW[0]/2
 
-Spec(np.transpose(x_IRF_1_1[:num])[0], f_IRF_1_1[0], title)
+low = fc - BW/2
+high = fc + BW/2
 
-file_to_load = 'baseline_1.mat'
-title = file_to_load.split('.')[0]
+## ERRADO: VER COMO APLICAR O FILTRO DE FREQ NESSE SINAL Q TA NO TEMPO (NO MATLAB TA ASSIM)
+x_ORF_2_Bpf = band_pass(np.transpose(x_ORF_2)[0], low, high, f_ORF_2[0], order=2)
 
-x_bsln_1, f_bsln_1, t_bsln_1, BSLN_DATA = Load(file_to_load)
-num = int(len(t_bsln_1)*0.0005)
-
-freq, psd, L = PSD(x_bsln_1, t_bsln_1, num)
-
-plot_psd(freq, psd, L, title)
-
-
-Spec(np.transpose(x_bsln_1[:num])[0], f_bsln_1[0], title)
-
-
-
-#Wavelet test
-
-wavelet='db1'
-scales = (1, len(x_bsln_1))
-coeffs = pywt.wavedec(x_bsln_1, wavelet)
-
-recon = wavelet_filter(coeffs, wavelet)
-title = "Recon baseline"
-
-freq, psd, L = PSD(recon, t_bsln_1, num)
-
-plot_psd(freq, psd, L, title)
-
-Spec(np.transpose(recon[:num])[0], f_bsln_1[0], title='Recon')
-
-p_env_inner, f_env_inner, x_env_inner = env_spectrum(np.transpose(x_IRF_1_1[:])[0],  f_IRF_1_1[0])
-
-plt.figure()
-plt.subplot(2, 1, 1)
-plt.xlim(0.04, 0.06)
-plt.plot(t_IRF_1_1, x_IRF_1_1)
-plt.xlabel('Time [s]')
-plt.ylabel('Acceletarion [g]')
-num = int(len(t_IRF_1_1)*0.001)
-plt.subplot(2, 1, 2)
-plt.plot(t_IRF_1_1[:len(x_env_inner)], x_env_inner)
-plt.xlim(0.04, 0.06)
-# plt.ylim(0, 1)
-plt.xlabel('Time [s]')
-plt.ylabel('Acceletarion [g]')
+p_env_ORF_2_Bpf, f_env_ORF_2_Bpf, x_env_ORF_2_Bpf, t_env_ORF_Bpf = env_spectrum(np.transpose(x_ORF_2)[0], f_ORF_2, t_ORF_2, BAND=True, LOW=low, HIGH=high)
 
 
-plt.figure()
-a = 3500
-plt.plot(f_env_inner[:a], p_env_inner[:a])
-plt.xlim(0, 1000)
-plt.ylim( 0, 0.5)
 
-ncomb = 10
-harmonics = [k * IRFDATA["BPFI"] for k in range(1, ncomb + 1)]
+# plot_env_spectrum(x_env, x_ORF_2, t_ORF_2)
+# plot_env_spectrum(x_env_ORF_2_Bpf, x_ORF_2_Bpf, t_ORF_2)
 
-plt.vlines(harmonics, 0, max(p_env_inner[:a]), colors='r', linestyles='--')
+
+# plot_env_spectrum_analysis(f_env_ORF_2_Bpf, p_env_ORF_2_Bpf, ORF_DATA, BPFO=True)
+
+high, _= hl_envelopes_idx(np.transpose(x_ORF_2)[0])
+# plot_env_spectrum(x_env, t_env, x_ORF_2, t_ORF_2, HIGH=high, xlim=[0, 0.1])
+# plt.ylim(-1, 1)
+
+
+# high, _= hl_envelopes_idx(x_ORF_2_Bpf)
+plot_env_spectrum(x_ORF_2_Bpf, t_env_ORF_Bpf, x_ORF_2, t_ORF_2, HIGH=high, xlim=[0, 0.1])
+# plt.ylim(-1, 1)
+
+plot_env_spectrum_analysis(f_env_ORF_2_Bpf, p_env_ORF_2_Bpf, ORF_DATA,BPFO=True)
+plt.ylim(0, 0.2)
 
 
 plt.show()
+
